713b055d3eba6c3c335b50acfff0a0b93c9e9f9e
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-04-29 19:07:51 +0200

    Ajedrez implementado a falta de mas informacion

---
+++ src/components/Chess.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="board1"></div>
+</template>
+
+<script>
+import ChessBoard from "chessboardjs-vue";
+export default {
+	data() {
+		return {
+			config: {
+				draggable: true,
+				dropOffBoard: "snapback", // this is the default
+				position: "start",
+			},
+		};
+	},
+	name: "Home",
+	components: { ChessBoard },
+
+	mounted() {
+		ChessBoard("board1", this.config);
+	},
+};
+</script>
+
+<style>
+#board1 {
+	width: 400px;
+}
+</style>
